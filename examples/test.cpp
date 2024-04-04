157c6fe4eb9d0173f18278e63da5b01630cc106c
vadymchan <vadymchan@gmail.com>
2024-04-04 11:41:54 +0300

    test CMake build

---
+++ examples/test.cpp
@@ -0,0 +1,19 @@
+#include <eigen_graphics/test.h>
+#include <iostream>
+
+int main()
+{
+    // Create two random matrices
+    Eigen::MatrixXd mat1 = Eigen::MatrixXd::Random(3, 3);
+    Eigen::MatrixXd mat2 = Eigen::MatrixXd::Random(3, 3);
+
+    // Perform matrix multiplication
+    Eigen::MatrixXd result = mat1 * mat2;
+
+    // Print the input matrices and the result
+    std::cout << "Matrix 1:\n" << mat1 << "\n\n";
+    std::cout << "Matrix 2:\n" << mat2 << "\n\n";
+    std::cout << "Multiplication result:\n" << result << "\n";
+
+    return 0;
+}
